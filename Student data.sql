3cbb3a58fa602e7bfbc63af81bef778875a5db58
Fahim <fahimyadgir07@gmail.com>
2026-07-24 11:04:18 +0530

    Update some column and add more column and add forein key in complaint table

--- Student data.sql
+++ Student data.sql
@@ -1,13 +1,17 @@
 create database student_complaint_portal;
 use student_complaint_portal;
 
+drop table Student;
+
 create table Student(
 Student_id int primary key,
-Std_Academic_info varchar (100) not null ,
+PRN_No int not null unique,
 Std_email varchar (50) not null unique,
-Std_Mobile_No bigint not null
-)
-
-
+Std_Mobile_No bigint not null,
+Department varchar (100) not null,
+Division varchar (100) not null,
+`Year-of-Study` int not null,
+`Password` varchar (100) not null
+);
 
 
--- complaints data.sql
+++ complaints data.sql
@@ -1,9 +1,15 @@
-create table complaints(
-complaint_id bigint unique,
-Title varchar (50) not null,
-Descriptions varchar (200) not null,
-Location varchar (50) not null,
-statuss varchar (50) not null,
-category varchar (100) not null,
-Documents_path varchar(300) not null
+use student_complaint_portal;
+
+drop table complaints;
+
+CREATE TABLE complaints (
+Comlpaint_id int primary key,
+Student_id int not null,
+Complaint_title varchar (100) not null,
+Complaint_description varchar (200) not null,
+Complaint_date timestamp default current_timestamp,
+Complaint_status enum('Inprogress','Resolve','Reject'),
+Admin_Reply varchar (100) ,
+Replay_date timestamp Not null,
+foreign key (Student_id) references Student (Student_id)
 );
